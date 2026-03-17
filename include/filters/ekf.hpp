3079a7bfa28fb87af79d76b2ad7c3a0de250a511
henraldo <henrysdixon01@gmail.com>
2026-03-17 00:41:20 -0400

    renamed variables

--- include/filters/ekf.hpp
+++ include/filters/ekf.hpp
@@ -13,10 +13,10 @@ struct EkfParams {
 
     EkfParams(
         const StateCov& p_initial = Eigen::Matrix<double, 6, 1>(5.0, 5.0, 2.0, 20.0, 20.0, 2.0).asDiagonal(),
-        const StateCov& q_initial = Eigen::Matrix<double, 6, 1>(0.001, 0.001, 3.25, 0.01, 0.01, 3.25).asDiagonal(),
-        const MeasCov& r_initial = Eigen::Matrix<double, 5, 1>(0.001, 3.0, 0.01, 2.0, 2.0).asDiagonal()
+        const StateCov& q_tune = Eigen::Matrix<double, 6, 1>(0.001, 0.001, 3.25, 0.01, 0.01, 3.25).asDiagonal(),
+        const MeasCov& r_tune = Eigen::Matrix<double, 5, 1>(0.001, 3.0, 0.01, 2.0, 2.0).asDiagonal()
     ) noexcept
-        : P0(p_initial), Q(q_initial), R(r_initial) {
+        : P0(p_initial), Q(q_tune), R(r_tune) {
             Omega = StateCov::Identity() * DT;
         }
 };
